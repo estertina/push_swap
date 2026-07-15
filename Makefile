b27922926b91684d4c1362ca5c17cfacd924962a
Ester <ester@MacBook-Pro-von-Ester.local>
2026-07-16 00:26:10 +0200

    completed main function, added free function, completed Makefile and headerfile

--- Makefile
+++ Makefile
@@ -6,33 +6,51 @@
 #    By: esttina <esttina@student.42heilbronn.de    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/23 17:23:47 by esttina           #+#    #+#              #
-#    Updated: 2026/01/23 17:24:51 by esttina          ###   ########.fr        #
+#    Updated: 2026/07/14 13:45:36 by esttina          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME = push_swap.a
+NAME = push_swap
 
 all: ${NAME}
 
-SRCS = 
+SRCS = 	srcs/index.c \
+		srcs/list_utils.c \
+		srcs/utils.c \
+		srcs/push.c \
+		srcs/radix.c \
+		srcs/rev_rotate.c \
+		srcs/rotate.c \
+		srcs/small_sort.c \
+		srcs/swap.c \
+		srcs/main.c
+
 
 OBJECTS = $(SRCS:.c=.o)
 
-FLAGS = -Wall -Wextra -Werror
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+FLAGS = -Wall -Wextra -Werror -I includes -I $(LIBFT_DIR)
 
 CC = cc
 
-%.o: %.c push_swap.h
+%.o: %.c includes/push_swap.h
 	${CC} ${FLAGS} -c $< -o $@
 
-$(NAME): $(OBJECTS)
-	ar rcs $(NAME) $(OBJECTS)
+$(LIBFT):
+	@make -C $(LIBFT_DIR)
+
+$(NAME): $(OBJECTS) $(LIBFT)
+	${CC} ${FLAGS} $(OBJECTS) $(LIBFT) -o $(NAME)
 
 clean:
 	rm -rf ${OBJECTS}
+	@make clean -C $(LIBFT_DIR)
 
 fclean:	clean
 	rm -rf ${NAME}
+	@make fclean -C $(LIBFT_DIR)
 
 re: fclean all
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,42 +6,57 @@
 /*   By: esttina <esttina@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 06:42:18 by esttina           #+#    #+#             */
-/*   Updated: 2026/07/14 12:11:20 by esttina          ###   ########.fr       */
+/*   Updated: 2026/07/16 00:04:36 by esttina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	init_stack(t_node **stack_a, int ac, char **av)
+void	free_split(char **str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return ;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+int	init_stack(t_node **stack, char **args)
 {
 	t_node	*new_node;
 	long	tmp;
 	int		i;
 
-	i = 1;
-	while (i < ac)
+	i = 0;
+	while (args[i])
 	{
-		if (check_validity(av[i]))
+		if (check_validity(args[i]))
 			return (1);
 
-		tmp = ft_atol(av[i]);
-		if (tmp > 2147483647 || tmp < -2147483648 || check_duplicate(*stack_a, (int)tmp));
+		tmp = ft_atol(args[i]);
+		if (tmp > 2147483647 || tmp < -2147483648 || check_duplicate(*stack, (int)tmp))
 			return (1);
 		
 		new_node = ft_lstnew((int)tmp);
 		if (!new_node)
 			return (1);
 		
-		ft_lstadd_back(stack_a, new_node);
+		ft_lstadd_back(stack, new_node);
 		i++;
-
-		return (0);
 	}
+	return (0);
 }
 int	main(int ac, char **av)
 {
 	t_node	*stack_a;
 	t_node	*stack_b;
+	char	**args;
 
 	if (ac < 2)
 		return (0);
@@ -49,10 +64,40 @@ int	main(int ac, char **av)
 	stack_a = NULL;
 	stack_b = NULL;
 	
-	if (init_stack(&stack_a, ac, av) == 1)
+	if (ac == 2)
+		args = ft_split(av[1], ' ');
+	else
+		args = av + 1;
+	
+	if (!args || !*args || init_stack(&stack_a, args) == 1)
 	{
 		write(2, "Error\n", 6);
+		free_stack(&stack_a);
+		if (ac == 2)
+			free_split(args);
 		return (1);
 	}
+
+	if (ac == 2)
+		free_split(args);
+	
+	if (is_sorted(&stack_a))
+	{
+		free_stack(&stack_a);
+		return (0);
+	}
+	
+	index_stack(&stack_a);
+	if (ft_lstsize(stack_a) == 2)
+		sa(&stack_a);
+	else if (ft_lstsize(stack_a) == 3)
+		sort_3(&stack_a);
+	else if (ft_lstsize(stack_a) <= 5)
+		sort_4_5(&stack_a, &stack_b);
+	else
+		radix_sort(&stack_a, &stack_b);
+
+	free_stack(&stack_a);
+	free_stack(&stack_b);	
 	return (0);
 }
